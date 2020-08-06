80920bc57fb709acaea72e3e997fae15b6368e6d
GKunc <34220464+GKunc@users.noreply.github.com>
2020-08-06 20:43:11 +0000

    Deploying to gh-pages from  @ fd1a5dc8ae2cc2cee0ff40ebf4ee7db93a84ac39 🚀

---
+++ src/app/api/repositories/PlaceRepository.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Place } from './../models/Place';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PlaceRepository {
+    places: Place[];
+    current_id: number;
+
+    constructor() {
+        this.places = [];
+        this.current_id = 1;
+    }
+
+    create(x: number, y: number): void {
+        let place: Place = new Place(this.current_id);
+        place.create(x, y);
+        this.current_id++;
+        this.places.push(place);
+    }
+
+    removeAll(): void {
+        this.places = [];
+        this.current_id = 1;
+    }
+
+    getAll(): Place[] {
+        return this.places;
+    }
+
+    getByID(id: number): Place {
+        return this.places.find(place => place.getID() === id);
+    }
+    
+    deleteElementByID(id: number): void {
+        let index = this.places.indexOf(this.getByID(id));
+        if (index !== -1) {
+            this.places.splice(index, 1);
+        }
+    }
+}
